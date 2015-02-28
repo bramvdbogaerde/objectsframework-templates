100525e1c064c61447c571dec01dc1eeaea33a51
Bram Vandenbogaerde <contact@bramvdb.com>
2015-02-28 11:58:05 +0100

    Initial commit

---
+++ lib/objectsframework/templates.rb
@@ -0,0 +1,29 @@
+module ObjectsFramework
+	# Add some methods to the ObjectsFramework Object class to enable easy rendering inside an object
+	class Object
+		def self.set_template_layout(layout)
+			@@layout = layout
+		end
+
+		def self.set_template_directory(dir)
+			@@directory = dir
+		end
+
+		def render(templatename)
+			Tilt::ERBTemplate.new(@@directory+"/"+@@layout).render(self) {Tilt::ERBTemplate.new(@@directory+"/"+templatename).render(self)}
+		end
+	end
+end
+
+# # Example Usage:
+# # Base path /user
+# class User < ObjectsFramework::Object
+# 	set_template_layout "layout.html.erb"
+# 	set_template_directory "views"
+#
+# 	# Request: GET /user/index or GET /user
+# 	def get_index
+# 		# Will render "views/index.html.erb" with layout "layout.html.erb"
+# 		response.write(render("index.html.erb"))
+# 	end
+# end
---
+++ objectsframework-templates.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'objectsframework-templates'
+  s.version     = '1.0.0'
+  s.licenses    = ['MIT']
+  s.summary     = "Templates Engine for ObjectsFramework"
+  s.description = "Templates Engine for ObjectsFramework, currently only ERB templating supported, coming is HAML and others!"
+  s.authors     = ["Bram Vandenbogaerde"]
+  s.email       = 'contact@bramvdb.com'
+  s.files       = Dir.glob("lib/**/**")
+  s.add_runtime_dependency "tilt","~> 2.0.1"
+  s.homepage    = 'https://github.com/bramvdbogaerde/objectsframework-templates'
+end
